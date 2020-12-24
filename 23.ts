b22599d7fd59ae5e3ee3c5747c968174898eee78
Luka Skukan <lskukan1@gmail.com>
2020-12-24 08:12:26 +0100

    Complete 23 and 24

--- 23.ts
+++ 23.ts
@@ -3,40 +3,84 @@ import { range } from "./utilityBelt";
 
 const input = '784235916'.split('').map(_ => _.toInt());
 
-function runRound(xs: number[]): number[] {
-  const target = [...xs];
-  const moved = target.splice(1, 3);
-  const current = target.shift()!;
-  const destinationCup = ((): number => {
-    let label = current - 1;
+type Current = number;
+type Next = number;
+interface Cup {
+  value: number;
+  next: Next;
+}
+type Cups = Map<Current, Cup>;
+
+function getCups(xs: number[]): Cups {
+  const cups: Cups = new Map();
+
+  xs.forEach((num, index) => {
+    const value = num;
+    const next = xs[(index + 1) % xs.length];
+    cups.set(value, { value, next });
+  });
+
+  return cups;
+}
+
+function runRoundMap(currentValue: Current, cups: Cups): Next {
+  const current = cups.get(currentValue)!
+  const first = cups.get(current.next)!;
+  const second = cups.get(first.next)!;
+  const third = cups.get(second.next)!;
+
+  const moved = [first.value, second.value, third.value];
+
+  const destinationValue = ((): number => {
+    let label = currentValue - 1;
     while (true) {
       if (moved.includes(label)) label--;
-      else if (label <= 0) label = Math.max(...target);
+      else if (label <= 0) label = cups.size;
       else return label;
     }
   })();
-  const insertionIndex = target.indexOf(destinationCup) + 1;
-  target.splice(insertionIndex, 0, ...moved);
-  target.push(current);
-  return target;
+
+  const destination = cups.get(destinationValue)!;
+  const afterDestination = destination.next;
+  current.next = third.next;
+  destination.next = first.value;
+  third.next = afterDestination;
+
+  return current.next;
+}
+
+function stringify(cups: Cups, startingValue: number) {
+  let output = String(startingValue);
+  let ptr = cups.get(cups.get(1)!.next)!;
+  while(ptr.value !== startingValue) {
+    output += ptr.value;
+    ptr = cups.get(ptr.next)!;
+  }
+  return output;
 }
 
 function main1() {
-  let cups = input;
+  const cups = getCups(input);
+  let current = input[0];
   for (let i = 0; i < 100; i++) {
-    cups = runRound(cups);
+    current = runRoundMap(current, cups);
   }
-  console.log(cups.join(''));
+  console.log(stringify(cups, 1).slice(1));
 }
 
 function main2() {
-  let cups = input.concat(range(Math.max(...input) + 1, 1_000_001));
+  const cups = getCups(input.concat(range(Math.max(...input) + 1, 1_000_001)));
+  let current = input[0];
   for (let i = 0; i < 10_000_000; i++) {
-    cups = runRound(cups);
+    current = runRoundMap(current, cups);
   }
-  const indexOfOne = cups.indexOf(1);
-  const [next, other] = [cups[indexOfOne + 1 % cups.length], cups[indexOfOne + 2 % cups.length]]
-  console.log(next, other, next * other);
+
+  const first = cups.get(cups.get(1)!.next)!;
+  const second = cups.get(first.next)!;
+  console.log(first.value, second.value, first.value * second.value);
 }
+
+// 53248976
+// main1();
 
 main2();
---
+++ 24.ts
@@ -0,0 +1,109 @@
+import { ArraySet } from "./ArraySet";
+import { count, flatten, readLines } from "./utilityBelt";
+
+
+type ITile = [number, number];
+
+function strToMove(str: string): ITile {
+  switch (str) {
+    case 'e': return [2, 0];
+    case 'w': return [-2, 0];
+    case 'nw': return [-1, 1];
+    case 'ne': return [1, 1];
+    case 'sw': return [-1, -1];
+    case 'se': return [1, -1];
+    default: throw new Error(`Unexpected input ${str}`);
+  }
+}
+
+function navigate(moves: ITile[], from: ITile = [0, 0]): ITile {
+  return moves.reduce((tile, move) => [tile[0] + move[0], tile[1] + move[1]], from);
+}
+
+function hexNeighbours(tile: ITile): ITile[] {
+  return ['e', 'w', 'ne', 'nw', 'se', 'sw'].map((dir) => navigate([strToMove(dir)], tile));
+}
+
+async function readTiles(): Promise<ITile[]> {
+  const lines = await readLines('./24.txt');
+  const tiles: ITile[] = [];
+
+  for (const line of lines) {
+    let buf: string = '';
+    const moves: ITile[] = [];
+
+    const chars = line.split('');
+
+    for (const char of chars) {
+      if (char === 'n' || char === 's') {
+        if (buf.length) {
+          moves.push(strToMove(buf));
+        }
+        buf = char;
+      } else {
+        if (buf.endsWith('e') || buf.endsWith('w')) {
+          moves.push(strToMove(buf));
+          buf = char;
+        } else {
+          buf += char;
+        }
+      }
+    }
+
+    if (buf.length) {
+      moves.push(strToMove(buf));
+    }
+
+    tiles.push(navigate(moves));
+  }
+
+  return tiles;
+}
+
+async function getInitialTiles() {
+  const tiles = await readTiles();
+  const flipped = new ArraySet<ITile>();
+
+  for (const tile of tiles) {
+    if (flipped.has(tile)) {
+      flipped.delete(tile);
+    } else {
+      flipped.add(tile);
+    }
+  }
+
+  return flipped;
+}
+
+async function main1() {
+  const flipped = await getInitialTiles();
+
+  console.log(flipped.size);
+}
+
+async function main2() {
+  let tiles = await getInitialTiles();
+
+  for (let i = 0; i < 100; i++) {
+    const next = new ArraySet<ITile>()
+    const candidates = flatten(tiles.toList().map((t) => [t, ...hexNeighbours(t)]));
+
+    for (const candidate of candidates) {
+      const isBlack = tiles.has(candidate);
+      const blackNeighbours = count(hexNeighbours(candidate), (t) => tiles.has(t));
+
+      if (isBlack) {
+        if (blackNeighbours > 0 && blackNeighbours <= 2) {
+          next.add(candidate);
+        }
+      } else if (blackNeighbours === 2) {
+        next.add(candidate);
+      }
+    }
+    tiles = next;
+  }
+
+  console.log(tiles.size);
+}
+
+main2();
