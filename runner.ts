0d5899153db9d72efac5713b2508cda021f0362a
Luka Skukan <luka.skukan@axilis.com>
2021-12-12 10:46:49 +0100

    Simplify runner and default to today if applicable

--- runner.ts
+++ runner.ts
@@ -8,19 +8,19 @@ import yargs from 'yargs/yargs';
 const parts = [1, 2] as const;
 
 yargs(process.argv.slice(2)).options({
-    day: { type: 'number', demandOption: true },
+    day: { type: 'number', default: new Date().getDate() },
     year: { type: 'number', default: new Date().getFullYear() },
     part: { choices: parts, demandOption: false },
-    inputFile: { type: 'string', demandOption: false },
+    input: { type: 'string', demandOption: false },
 }).parseAsync().then((args) => {
     const solutionPath = join(__dirname, args.year.toString(), `${args.day}.ts`);
+    const inputFileName = args.input ?? `${args.day}.input`;
+    console.log(`# Running AoC ${args.year} / Day ${args.day} | Input: ${inputFileName}`);
 
     import(solutionPath).then((imported: { default: DaySolution<any> }) => {
         const task = imported.default;
         
-        const inputFile = args.inputFile == null
-            ? join(__dirname, args.year.toString(), `${args.day}.input`)
-            : join(__dirname, args.year.toString(), args.inputFile!);
+        const inputFile = join(__dirname, args.year.toString(), inputFileName);
         
         const input: Promise<string | string[]> = task.lines
             ? readLines(inputFile)
@@ -28,12 +28,15 @@ yargs(process.argv.slice(2)).options({
 
         input.then((parsedInput: any) => {
             if (args.part === 2) {
+                console.log('# Part 2');
                 return task.part2!(parsedInput);
             } else if (args.part === 1) {
+                console.log('# Part 1');
                 return task.part1(parsedInput);
             } else if (task.part2 != null) {
                 return task.part2!(parsedInput);
             } else {
+                console.log('# Part 1');
                 return task.part1(parsedInput);
             }
         }).then((solution) => {
